acae5ae3c64dd61a27d4e70c2dbfaee438732dde
Vyacheslav Kromsky <thegarrulousmouth@gmail.com>
2022-08-18 20:19:54 +0500

    Оформление и поиск
    Созданые модели продуктов и категории. Сделано базовое оформление
    различных страниц, включая главную страницу, страницу с товарами.
    Сделан поиск по товарам

--- onlinestore/settings.py
+++ onlinestore/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'shop.context_processors.menu_links',
             ],
         },
     },
@@ -115,6 +116,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,34 @@
-from django.shortcuts import render, redirect
-from django.views.generic import TemplateView, CreateView
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import TemplateView, CreateView, FormView, DetailView, ListView
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import Group, User
 from .forms import SugnUpForm
+from .models import Category, Product as ProductModel
 
-class HomeView(TemplateView):
+class HomeView(ListView):
     template_name = 'shop/home.html'
+    paginate_by = 9
+
+    def get_queryset(self):
+        query = self.request.GET.get('search-product')
+        if query is not None:
+            return ProductModel.objects.filter(name__icontains=query)
+        try:
+            category_slug = self.kwargs['category_slug']
+            category_page = get_object_or_404(Category, slug=category_slug)
+            return ProductModel.objects.filter(category=category_page, available=True)
+        except KeyError:
+            return ProductModel.objects.all()
+
+
+class Product(DetailView):
+    def get(self, request, category_slug, product_slug):
+        try:
+            product = ProductModel.objects.get(category__slug=category_slug, slug=product_slug)
+        except Exception as e:
+            raise e
+        return render(request, 'shop/product.html', {'product': product})
 
 class SignUp(CreateView):
     form_class = SugnUpForm
@@ -26,7 +48,7 @@ class SignUp(CreateView):
             return redirect('home')
         return render(request, self.template_name, {'form': form})
 
-class Login(CreateView):
+class Login(FormView):
     form_class = AuthenticationForm
     template_name = 'registration/login.html'
     def get(self, request):
@@ -45,7 +67,7 @@ class Login(CreateView):
             else:
                 return redirect('signup')
 
-class Logout(CreateView):
+class Logout(TemplateView):
 
     def get(self, request):
         logout(request)
